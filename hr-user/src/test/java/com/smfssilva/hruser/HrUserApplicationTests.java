48a9d573d02dc241537840f90245e3859c01f31b
smfssilva <smfssilva@yahoo.com>
2021-10-08 20:27:19 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/smfssilva/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smfssilva.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
