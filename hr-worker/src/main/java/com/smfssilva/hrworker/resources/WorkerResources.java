8208dcf4857ac4d3dd1a089e6ab960a35f3e0efe
smfssilva <smfssilva@yahoo.com>
2021-10-08 15:36:48 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/smfssilva/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/smfssilva/hrworker/resources/WorkerResources.java
@@ -1,28 +1,45 @@
 package com.smfssilva.hrworker.resources;
 
-import com.smfssilva.hrworker.entities.Worker;
-import com.smfssilva.hrworker.repositories.WorkerRepository;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.smfssilva.hrworker.entities.Worker;
+import com.smfssilva.hrworker.repositories.WorkerRepository;
 
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResources {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
 
    @Autowired
    private WorkerRepository repository;
 
+   @GetMapping(value = "/configs")
+   public ResponseEntity<Void> getConfigs() {
+      logger.info("CONFIG = " + testConfig);
+
+      return ResponseEntity.noContent().build();
+   }
+   
    @GetMapping
    public ResponseEntity<List<Worker>> findAll() {
-      List<Worker> list = repository.findAll();
-
-      return ResponseEntity.ok(list);
+	   List<Worker> list = repository.findAll();
+	   
+	   return ResponseEntity.ok(list);
    }
 
    @GetMapping(value = "/{id}")
