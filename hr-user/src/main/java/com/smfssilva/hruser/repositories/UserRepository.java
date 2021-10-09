d32dd13095a7499477360945c98ead6c4be70216
smfssilva <smfssilva@yahoo.com>
2021-10-08 21:28:41 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/smfssilva/hruser/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.smfssilva.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.smfssilva.hruser.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
+
+}
